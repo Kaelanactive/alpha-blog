bd216c71dd574e7920c7f66bd9401b1dd762f80b
Kaelanactive <Littledragon1022@gmail.com>
2016-03-08 14:49:51 +0000

    Add edit update funtionality and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-   @user = User.new(user_params)
+   @user = User.new
     if @user.save
       flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
       redirect_to articles_path
@@ -13,9 +13,18 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-  def user_params
-    params.require(:user). permit(:username, :email, :password)
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+ else
+      render 'edit'
+    end
   end
 
 end
